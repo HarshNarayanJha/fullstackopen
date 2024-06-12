8cd982c747f6d6e9f0cf29f21051d185e1fd4326
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-06-12 20:48:19 +0530

    exercise 1.1 courseinfo step 1

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,26 +1,50 @@
-import './App.css'
-
-const Hello = (props) => {
-  console.log(props)
+const Header = (props) => {
   return (
     <>
-      <p>Hello { props.name } with { props.age } years of breathing experience!</p>
+      <h1>{props.course}</h1>
     </>
   )
 }
 
-function App() {
-
-  const name = "Peach"
-  const age = 20
-
+const Content = (props) => {
   return (
     <>
-      <h1>Greetings</h1>
-      <Hello name="Mario" age={ 10 } />
-      <Hello name="Luigi" age = { age - 15 }/>
-      <Hello name={ name } age={ age } />
+      <p>
+        {props.part1} {props.exercises1}
+      </p>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+      <p>
+        {props.part3} {props.exercises3}
+      </p>
     </>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <>
+      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    </>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course}></Header>
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}></Content>
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}></Total>
+    </div>
   )
 }
 
