7e9dbcef395275fad5858e14cf33993df338ca07
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-06-13 14:37:32 +0530

    exercise 1.6 unicafe step 1

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,7 +8,19 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <h1>Give Feedback</h1>
+
+      <div>
+        <button onClick={ () => setGood(good + 1) }>Good</button> &nbsp;&nbsp;
+        <button onClick={ () => setNeutral(neutral + 1) }>Neutral</button> &nbsp;&nbsp;
+        <button onClick={ () => setBad(bad + 1) }>Bad</button> &nbsp;&nbsp;
+      </div>
+
+      <h2>Statistics</h2>
+
+      <div>Good { good }</div>
+      <div>Neutral { neutral }</div>
+      <div>Bad { bad }</div>
     </div>
   )
 }
