49829776e869aefa80c2fa7531fe0df5c9604792
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-06-13 15:36:33 +0530

    anectodes step 1 complete

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -11,12 +11,17 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
   ]
-   
+
   const [selected, setSelected] = useState(0)
+
+  const getRandomIndex = (i) => {
+    return Math.floor(Math.random() * i)
+  }
 
   return (
     <div>
-      {anecdotes[selected]}
+      <div>{anecdotes[selected]}</div>
+      <button onClick={() => { setSelected(getRandomIndex(anecdotes.length)) }}>next anectode</button>
     </div>
   )
 }
