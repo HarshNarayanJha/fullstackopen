de7f68252811b369ceef9c0bf523f1258943f29a
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-06-13 15:03:15 +0530

    exercise 1.8 unicafe step 3

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,37 +1,50 @@
 import { useState } from 'react'
 
+const Statistics = (props) => {
+
+  const { good, neutral, bad } = props
+
+  const getTotalFeedbacks = () => good + neutral + bad
+  const getAverageFeedback = () => {
+    if (getTotalFeedbacks() == 0) return 0
+    return (good - bad) / getTotalFeedbacks();
+  }
+  const getPositiveFeedback = () => {
+    if (getTotalFeedbacks() == 0) return 0
+    return good / getTotalFeedbacks() * 100;
+  }
+
+  return (
+    <>
+      <h2>Statistics</h2>
+      
+      <div>Good {good}</div>
+      <div>Neutral {neutral}</div>
+      <div>Bad {bad}</div>
+      <div>All {getTotalFeedbacks()}</div>
+      <div>Average {getAverageFeedback()}</div>
+      <div>Positive {getPositiveFeedback()} %</div>
+    </>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const getTotalFeedbacks = () => good + neutral + bad
-  const getAverageFeedback = () => {
-    return (good - bad) / getTotalFeedbacks();
-  }
-  const getPositiveFeedback = () => {
-    return good / getTotalFeedbacks() * 100;
-  }
-
   return (
     <div>
       <h1>Give Feedback</h1>
 
       <div>
-        <button onClick={ () => setGood(good + 1) }>Good</button> &nbsp;&nbsp;
-        <button onClick={ () => setNeutral(neutral + 1) }>Neutral</button> &nbsp;&nbsp;
-        <button onClick={ () => setBad(bad + 1) }>Bad</button> &nbsp;&nbsp;
+        <button onClick={() => setGood(good + 1)}>Good</button> &nbsp;&nbsp;
+        <button onClick={() => setNeutral(neutral + 1)}>Neutral</button> &nbsp;&nbsp;
+        <button onClick={() => setBad(bad + 1)}>Bad</button> &nbsp;&nbsp;
       </div>
 
-      <h2>Statistics</h2>
-
-      <div>Good { good }</div>
-      <div>Neutral { neutral }</div>
-      <div>Bad { bad }</div>
-      <div>All { getTotalFeedbacks() }</div>
-      <div>Average { getAverageFeedback() }</div>
-      <div>Positive { getPositiveFeedback() } %</div>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
