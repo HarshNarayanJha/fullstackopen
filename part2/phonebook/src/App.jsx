22329482ce3d5578c5db44093a0a1b86711c5f3c
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-06-14 16:47:58 +0530

    exercise 2.9 phonebook step 4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,35 +2,58 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '+919876543210' }
+    { name: 'Arto Hellas', phone: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', phone: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', phone: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', phone: '39-23-6423122', id: 4 }
   ])
+
+  const [filteredPersons, setFilteredPersons] = useState(persons)
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
 
+  const [searchName, setSearchName] = useState('')
+
   const handleSubmit = (e) => {
     e.preventDefault()
-    if (persons.reduce((acc, person) => { return acc + (person.name === newName || person.phone === newPhone) ? 1 : 0 }, 0) == 0) {
+    const alreadyIn = persons.reduce((acc, person) => { return acc + (person.name === newName) ? 1 : 0 + (person.phone === newPhone) ? -1 : 0 }, 0);
+    if (alreadyIn == 0) {
       setPersons(persons.concat({ name: newName, phone: newPhone }))
+      setFilteredPersons(persons.concat({ name: newName, phone: newPhone }))
       setNewName('')
       setNewPhone('')
+      setSearchName('')
     } else {
-      alert(`${newName} or ${newPhone} is already added to phonebook`)
+      alert(`${alreadyIn > 0 ? newName : newPhone} is already added to phonebook`)
     }
   }
 
   const handleNewName = (e) => {
-    console.log(e.target.value)
     setNewName(e.target.value)
   }
 
   const handleNewPhone = (e) => {
-    console.log(e.target.value)
     setNewPhone(e.target.value)
+  }
+
+  const handleSearchName = (e) => {
+    setSearchName(e.target.value)
+    setFilteredPersons(persons.filter((person) => {
+      if (e.target.value === '') {
+        return true
+      }
+      
+      return person.name.toLowerCase().includes(e.target.value)
+    }))
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with: <input value={searchName} onChange={handleSearchName} required />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input value={newName} onChange={handleNewName} type='name' required />
@@ -43,7 +66,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <li key={person.name}>{person.name} {person.phone}</li>)}
+      {filteredPersons.map(person => <li key={person.name}>{person.name} {person.phone}</li>)}
     </div>
   )
 }
