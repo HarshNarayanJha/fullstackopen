ce7cdc4c2f966586329a7cffcc395f3b2aba8f9e
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-06-14 16:14:48 +0530

    exercise 2.7 phonebook step 2

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -8,8 +8,12 @@ const App = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    setPersons(persons.concat({name: newName}))
-    setNewName('')
+    if (persons.reduce((acc, person) => {return acc + (person.name === newName) ? 1 : 0}, 0) == 0) {
+      setPersons(persons.concat({name: newName}))
+      setNewName('')
+    } else {
+      alert(`${newName} is already added to phonebook`)
+    }
   }
 
   const handleNewName = (e) => {
