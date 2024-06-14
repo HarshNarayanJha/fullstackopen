7e12a95111fe22d3eb3fb5df16a720dfb706d90e
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-06-14 15:31:07 +0530

    exercise 2.5 courseinfo step 10

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,28 +1,4 @@
-const Header = ({ course }) => <h2>{course}</h2>
-
-const Part = ({ part }) =>
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Total = (props) => <b> total of {props.parts.reduce((total, part) => { return total + part.exercises }, 0)} exercises</b>
-
-const Content = ({ parts }) => parts.map(part => <Part key={part.id} part={part} />)
-
-const Course = ({ courses }) => {
-  return (
-    <div>
-      <h1>Web Development Cirriculum</h1>
-      {courses.map(course => {
-        return (<>
-          <Header course={course.name} />
-          <Content parts={course.parts} />
-          <Total parts={course.parts} />
-        </>)
-      })}
-    </div>
-  )
-}
+import Course from "./components/Course"
 
 const App = () => {
   const courses = [
