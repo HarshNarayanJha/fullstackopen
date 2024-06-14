ad46a48d68e8d0d4768c1911a5e45d8073b7d0ad
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-06-14 15:23:40 +0530

    exercise 2.3 courseinfo step 8

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -5,7 +5,7 @@ const Part = ({ part }) =>
     {part.name} {part.exercises}
   </p>
 
-const Total = (props) => <b> total of {props.parts.reduce((total, part) => { return total + part.exercises }, 0)} exercises</b>
+const Total = (props) => <b> total of { props.parts.reduce((total, part) => { return total + part.exercises }, 0) } exercises</b>
 
 const Content = ({ parts }) => parts.map(part => <Part key={part.id} part={part} />)
 
