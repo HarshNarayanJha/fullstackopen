f1b66aba65b7a17679b5c6d202eac3589e673c6d
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-06-16 12:45:50 +0530

    exercise 2.11 phonebook step 6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,31 +1,31 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 import Filter from './components/Filter'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', phone: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', phone: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', phone: '39-23-6423122', id: 4 }
-  ])
-
-  const [filteredPersons, setFilteredPersons] = useState(persons)
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
 
   const [searchName, setSearchName] = useState('')
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((response) => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const handleSubmit = (e) => {
     e.preventDefault()
     const alreadyIn = persons.reduce((acc, person) => { return acc + (person.name === newName) ? 1 : 0 + (person.phone === newPhone) ? -1 : 0 }, 0);
     if (alreadyIn == 0) {
       setPersons(persons.concat({ name: newName, phone: newPhone }))
-      setFilteredPersons(persons.concat({ name: newName, phone: newPhone }))
       setNewName('')
       setNewPhone('')
-      setSearchName('')
     } else {
       alert(`${alreadyIn > 0 ? newName : newPhone} is already added to phonebook`)
     }
@@ -41,14 +41,12 @@ const App = () => {
 
   const handleSearchName = (e) => {
     setSearchName(e.target.value)
-    setFilteredPersons(persons.filter((person) => {
-      if (e.target.value === '') {
-        return true
-      }
-
-      return person.name.toLowerCase().includes(e.target.value)
-    }))
   }
+
+  const searchPersons =
+    searchName === ''
+      ? persons
+      : persons.filter(person => person.name.toLowerCase().includes(searchName.toLowerCase()))
 
   return (
     <div>
@@ -62,7 +60,7 @@ const App = () => {
 
       <h3>Numbers</h3>
 
-      <Persons filteredPersons={filteredPersons} />
+      <Persons persons={searchPersons} />
     </div>
   )
 }
--- part2/phonebook/src/components/Persons.jsx
+++ part2/phonebook/src/components/Persons.jsx
@@ -1,5 +1,5 @@
 const Persons = (props) => {
-    return props.filteredPersons.map(person => <li key={person.name}>{person.name} {person.phone}</li>)
+    return props.persons.map(person => <li key={person.name}>{person.name} {person.phone}</li>)
 }
 
 export default Persons
