b36948e3ae177d87140f8abe2c8e8ee68557bef6
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-07-12 17:22:09 +0530

    4.5 Blog List step 5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -10,7 +10,29 @@ const totalLikes = blogs => {
   return blogs.reduce(reducer, 0)
 }
 
+const favouriteBlog = blogs => {
+
+  if (blogs.length === 0) {
+    return null;
+  }
+
+  var mostLikedBlog = blogs[0]
+
+  for (var blog of blogs) {
+    if (mostLikedBlog.likes < blog.likes) {
+      mostLikedBlog = blog
+    }
+  }
+
+  delete mostLikedBlog._id
+  delete mostLikedBlog.__v
+  delete mostLikedBlog.url
+
+  return mostLikedBlog
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favouriteBlog
 }
