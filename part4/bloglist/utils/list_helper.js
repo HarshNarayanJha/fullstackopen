ec4cec408150188ad7b77f9b3e041b14068a1fea
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-07-12 16:54:57 +0530

    4.4 Blog List step 4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = blogs => {
   return 1
 }
 
+const totalLikes = blogs => {
+  const reducer = (total, item) => {
+    return total + item.likes
+  }
+
+  return blogs.reduce(reducer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
