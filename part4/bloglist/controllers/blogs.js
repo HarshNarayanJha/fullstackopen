e083b66b05654aedc3e89cad9651431653adfab8
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-10-13 21:12:55 +0530

    4.22: Blog List Expansion, step 10

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,5 +1,4 @@
 const blogsRouter = require("express").Router()
-const jwt = require("jsonwebtoken")
 const Blog = require("../models/blog")
 const User = require("../models/user")
 
@@ -11,39 +10,29 @@ blogsRouter.get("/", async (request, response) => {
 blogsRouter.post("/", async (request, response, next) => {
   const body = request.body
 
-  try {
-    const decodedToken = jwt.verify(request.token, process.env.SECRET)
-    if (!decodedToken.id) {
-      return response.status(401).json({ error: "token invalid" })
-    }
+  const user = request.user
 
-    const user = await User.findById(decodedToken.id)
-
-    if (body.title == undefined) {
-      return response.status(400).json({ error: "title missing" })
-    }
-
-    if (body.url == undefined) {
-      return response.status(400).json({ error: "url missing" })
-    }
-
-    const blog = new Blog({
-      title: body.title,
-      author: body.author,
-      url: body.url,
-      user: user._id,
-    })
-
-    const savedBlog = await blog.save()
-
-    user.blogs = user.blogs.concat(savedBlog._id)
-    await user.save()
-
-    response.status(201).json(savedBlog)
-  } catch (error) {
-    // console.log(error)
-    return response.status(401).json({ error: "token invalid" })
+  if (body.title == undefined) {
+    return response.status(400).json({ error: "title missing" })
   }
+
+  if (body.url == undefined) {
+    return response.status(400).json({ error: "url missing" })
+  }
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    user: user._id,
+  })
+
+  const savedBlog = await blog.save()
+
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
+  response.status(201).json(savedBlog)
 })
 
 blogsRouter.get("/:id", async (request, response) => {
@@ -56,62 +45,41 @@ blogsRouter.get("/:id", async (request, response) => {
 })
 
 blogsRouter.put("/:id", async (request, response) => {
+  const blogId = request.params.id
+
   const body = request.body
 
-  try {
-    const decodedToken = jwt.verify(request.token, process.env.SECRET)
-    if (!decodedToken.id) {
-      return response.status(401).json({ error: "token invalid" })
+  const user = request.user
+
+  const blogToUpdate = await Blog.findById(blogId)
+
+  if (blogToUpdate.user.toString() === user._id.toString()) {
+    const blog = {
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes,
     }
 
-    const user = await User.findById(decodedToken.id)
-
-    const blogToUpdate = await Blog.findById(blogId)
-
-    if (blogToUpdate.user.toString() === user._id.toString()) {
-      const blog = {
-        title: body.title,
-        author: body.author,
-        url: body.url,
-        likes: body.likes,
-      }
-
-      const updatedBlog = await blogToUpdate.update(blog)
-      return response.json(updatedBlog)
-
-    } else {
-      return response.status(401).json({ error: "not your blog" })
-    }
-
-  } catch (error) {
-    return response.status(401).json({ error: "token invalid" })
+    const updatedBlog = await Blog.findByIdAndUpdate(blogId, blog, { new: true })
+    return response.json(updatedBlog)
+  } else {
+    return response.status(401).json({ error: "not your blog" })
   }
 })
 
 blogsRouter.delete("/:id", async (request, response) => {
   const blogId = request.params.id
 
-  try {
-    const decodedToken = jwt.verify(request.token, process.env.SECRET)
-    if (!decodedToken.id) {
-      return response.status(401).json({ error: "token invalid" })
-    }
+  const user = request.user
 
-    const user = await User.findById(decodedToken.id)
+  const blogToDelete = await Blog.findById(blogId)
 
-    const blogToDelete = await Blog.findById(blogId)
-
-    if (blogToDelete.user.toString() === user._id.toString()) {
-
-      await Blog.findByIdAndDelete(blogId)
-      return response.status(204).end()
-
-    } else {
-      return response.status(401).json({ error: "not your blog" })
-    }
-
-  } catch (error) {
-    return response.status(401).json({ error: "token invalid" })
+  if (blogToDelete.user.toString() === user._id.toString()) {
+    await Blog.findByIdAndDelete(blogId)
+    return response.status(204).end()
+  } else {
+    return response.status(401).json({ error: "not your blog" })
   }
 })
 
--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -1,4 +1,6 @@
 const logger = require("./logger")
+const jwt = require("jsonwebtoken")
+const User = require("../models/user")
 
 const requestLogger = (request, response, next) => {
   logger.info("Method:", request.method)
@@ -16,6 +18,24 @@ const tokenExtractor = (request, response, next) => {
   } else {
     logger.info("Invalid Authorization Header: ", authorization)
     request.token = null
+  }
+
+  next()
+}
+
+const userExtractor = async (request, response, next) => {
+  try {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: "token invalid" })
+    }
+
+    const user = await User.findById(decodedToken.id)
+    request.user = user
+  } catch (error) {
+    logger.error(error)
+    return response.status(401).json({ error: "token invalid" })
   }
 
   next()
@@ -46,6 +66,7 @@ const errorHandler = (error, request, response, next) => {
 module.exports = {
   requestLogger,
   tokenExtractor,
+  userExtractor,
   unknownEndpoint,
   errorHandler,
 }
