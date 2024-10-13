1da95c60489baa1af58ee88202b1f84af071632d
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-10-13 20:30:47 +0530

    4.21: Blog List Expansion, step 9

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -40,7 +40,6 @@ blogsRouter.post("/", async (request, response, next) => {
     await user.save()
 
     response.status(201).json(savedBlog)
-
   } catch (error) {
     // console.log(error)
     return response.status(401).json({ error: "token invalid" })
@@ -59,21 +58,61 @@ blogsRouter.get("/:id", async (request, response) => {
 blogsRouter.put("/:id", async (request, response) => {
   const body = request.body
 
-  const blog = {
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes,
+  try {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: "token invalid" })
+    }
+
+    const user = await User.findById(decodedToken.id)
+
+    const blogToUpdate = await Blog.findById(blogId)
+
+    if (blogToUpdate.user.toString() === user._id.toString()) {
+      const blog = {
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes,
+      }
+
+      const updatedBlog = await blogToUpdate.update(blog)
+      return response.json(updatedBlog)
+
+    } else {
+      return response.status(401).json({ error: "not your blog" })
+    }
+
+  } catch (error) {
+    return response.status(401).json({ error: "token invalid" })
   }
-
-  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
-
-  response.json(updatedBlog)
 })
 
 blogsRouter.delete("/:id", async (request, response) => {
-  await Blog.findByIdAndDelete(request.params.id)
-  return response.status(204).end()
+  const blogId = request.params.id
+
+  try {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: "token invalid" })
+    }
+
+    const user = await User.findById(decodedToken.id)
+
+    const blogToDelete = await Blog.findById(blogId)
+
+    if (blogToDelete.user.toString() === user._id.toString()) {
+
+      await Blog.findByIdAndDelete(blogId)
+      return response.status(204).end()
+
+    } else {
+      return response.status(401).json({ error: "not your blog" })
+    }
+
+  } catch (error) {
+    return response.status(401).json({ error: "token invalid" })
+  }
 })
 
 module.exports = blogsRouter
