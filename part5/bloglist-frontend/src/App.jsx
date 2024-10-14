b8c0d18ddb891d22f9a7375552a136bee11a8775
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-10-14 07:19:48 +0530

    5.3 & 5.4: Blog List Frontend, step 3 & 4

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -6,13 +6,23 @@ import loginService from "./services/login"
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [successMessage, setSuccessMessage] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
   const [user, setUser] = useState(null)
 
+  const [newTitle, setNewTitle] = useState("")
+  const [newAuthor, setNewAuthor] = useState("")
+  const [newUrl, setNewUrl] = useState("")
+
+  const fetchBlogs = async () => {
+    const blogs = await blogService.getAll()
+    setBlogs(blogs)
+  }
+
   useEffect(() => {
-    blogService.getAll().then(blogs => setBlogs(blogs))
+    fetchBlogs()
   }, [])
 
   useEffect(() => {
@@ -20,7 +30,7 @@ const App = () => {
     if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON)
       setUser(user)
-      // blogService.setToken(user.token)
+      blogService.setToken(user.token)
     }
   }, [])
 
@@ -31,22 +41,38 @@ const App = () => {
       const user = await loginService.login({ username, password })
       window.localStorage.setItem("loggedBlogUser", JSON.stringify(user))
       setUser(user)
-      // blogService.setToken(user.toekn)
+      blogService.setToken(user.token)
       setUsername("")
       setPassword("")
     } catch (exception) {
       console.log(exception)
-      setErrorMessage("wrong credentials")
+      setErrorMessage("wrong username or password")
+      setPassword("")
       setTimeout(() => {
         setErrorMessage(null)
       }, 3000)
     }
   }
 
-  const handleLogout = (e) => {
+  const handleLogout = e => {
     e.preventDefault()
-    window.localStorage.removeItem('loggedBlogUser')
+    window.localStorage.removeItem("loggedBlogUser")
     window.location.reload()
+  }
+
+  const addBlog = async e => {
+    e.preventDefault()
+    const newBlog = await blogService.create({ title: newTitle, author: newAuthor, url: newUrl })
+    setNewTitle("")
+    setNewAuthor("")
+    setNewUrl("")
+
+    await fetchBlogs()
+
+    setSuccessMessage(`Blog ${newBlog.title} by ${newBlog.author} Created Succesfully!`)
+    setTimeout(() => {
+      setSuccessMessage(null)
+    }, 3000)
   }
 
   const loginForm = () => (
@@ -77,13 +103,39 @@ const App = () => {
     </form>
   )
 
+  const blogForm = () => (
+    <form onSubmit={addBlog}>
+      Title: <input value={newTitle} onChange={({ target }) => setNewTitle(target.value)} />
+      <br />
+      Author: <input value={newAuthor} onChange={({ target }) => setNewAuthor(target.value)} />
+      <br />
+      Url: <input value={newUrl} onChange={({ target }) => setNewUrl(target.value)} />
+      <br />
+      <button type="submit">Save</button>
+      <br />
+      <br />
+    </form>
+  )
+
   return (
     <div>
       <h1>blogs</h1>
       <Notification message={errorMessage} messageType={"error"} />
+      <Notification message={successMessage} messageType={"success"} />
 
       {user === null && loginForm()}
-      {user !== null && <p>{user.name} logged-in <button onClick={handleLogout}>logout</button></p>}
+      {user !== null && (
+        <p>
+          {user.name} logged-in <button onClick={handleLogout}>logout</button>
+        </p>
+      )}
+
+      {user !== null && (
+        <div>
+          <h2>Create New</h2>
+          {blogForm()}
+        </div>
+      )}
 
       {blogs.map(blog => (
         <Blog key={blog.id} blog={blog} />
