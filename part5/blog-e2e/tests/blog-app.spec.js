fa43e6af16c10e51cf264bfd2dca55ace56e5afc
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-10-27 21:02:54 +0530

    5.18: Blog List End To End Testing, step 2

--- part5/blog-e2e/tests/blog-app.spec.js
+++ part5/blog-e2e/tests/blog-app.spec.js
@@ -1,12 +1,40 @@
 import { test, describe, beforeEach, expect } from "@playwright/test"
 
 describe("Blog app", () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post("http://localhost:3003/api/testing/reset")
+    await request.post("http://localhost:3003/api/users", {
+      data: {
+        name: "Mario",
+        username: "mario",
+        password: "zelda",
+      },
+    })
     await page.goto("http://localhost:5173")
   })
 
   test("Login form is shown", async ({ page }) => {
     await page.getByRole("button", { name: "Login" }).click()
-    await expect(page.getByRole('button', { name: 'login' })).toBeVisible()
+    await expect(page.getByRole("button", { name: "login" })).toBeVisible()
+  })
+
+  describe("Login", () => {
+    test("succeeds with correct credentials", async ({ page }) => {
+      await page.getByRole("button", { name: "Login" }).click()
+      await page.getByTestId("username").fill("mario")
+      await page.getByTestId("password").fill("zelda")
+      page.getByRole("button", { name: "login" }).click()
+
+      await expect(page.getByText("mario logged in")).toBeVisible()
+    })
+
+    test("fails with wrong credentials", async ({ page }) => {
+      await page.getByRole("button", { name: "Login" }).click()
+      await page.getByTestId("username").fill("mario2")
+      await page.getByTestId("password").fill("wrongzelda")
+      page.getByRole("button", { name: "login" }).click()
+
+      await expect(page.getByText("wrong username or password")).toBeVisible()
+    })
   })
 })
