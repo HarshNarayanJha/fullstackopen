ff356eeb8c3ca08f66b0398e5e34f1569bea7061
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-10-27 20:58:49 +0530

    update blog backend to add testing endpoints

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,36 +1,42 @@
-const config = require('./utils/config')
-const express = require('express')
+const config = require("./utils/config")
+const express = require("express")
 const app = express()
-const cors = require('cors')
-const blogsRouter = require('./controllers/blogs')
-const usersRouter = require('./controllers/users')
-const loginRouter = require('./controllers/login')
-const middleware = require('./utils/middleware')
-const logger = require('./utils/logger')
-const mongoose = require('mongoose')
+const cors = require("cors")
+const blogsRouter = require("./controllers/blogs")
+const usersRouter = require("./controllers/users")
+const loginRouter = require("./controllers/login")
+const middleware = require("./utils/middleware")
+const logger = require("./utils/logger")
+const mongoose = require("mongoose")
 
-mongoose.set('strictQuery', false)
+mongoose.set("strictQuery", false)
 
-logger.info('connecting to', config.MONGODB_URL)
+logger.info("connecting to", config.MONGODB_URL)
 
-mongoose.connect(config.MONGODB_URL)
+mongoose
+  .connect(config.MONGODB_URL)
   .then(() => {
-    logger.info('connected to MongoDB')
+    logger.info("connected to MongoDB")
   })
-  .catch((error) => {
-    logger.error('error connecting to MongoDB:', error.message)
+  .catch(error => {
+    logger.error("error connecting to MongoDB:", error.message)
   })
 
 app.use(cors())
-app.use(express.static('dist'))
+app.use(express.static("dist"))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use(middleware.tokenExtractor)
 
-app.use('/api/blogs', blogsRouter)
-app.use('/api/users', usersRouter)
-app.use('/api/login', loginRouter)
+app.use("/api/blogs", blogsRouter)
+app.use("/api/users", usersRouter)
+app.use("/api/login", loginRouter)
+
+if (process.env.NODE_ENV === "test") {
+  const testingRouter = require("./controllers/testing")
+  app.use("/api/testing", testingRouter)
+}
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
